3e96e372bfda59d31be74002a8496924abbc9204
codeaustin <fluxsor@gmail.com>
2022-10-16 16:05:29 -0400

    Added geolocation to get weather of user's location, added reverse geolocation to show city and state of location.

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -1,19 +1,36 @@
 import "./Weather.scss";
 import "./../../GlobalStyles.scss";
-import { getDailyWeather } from '../../api/index';
+import { getWeather, getLocationFromCoords } from '../../api/index';
 import { useEffect, useState } from "react";
 
 export const Weather = () => {
-    const[currentWeather, setCurrentWeather] = useState({});
+    const[allWeather, setallWeather] = useState({});
+    const[dailyWeather, setDailyWeather] = useState({});
     const[isLoading, setIsLoading] = useState(true);
+    const[locationFromCoords, setLocationFromCoords] = useState({});
+    const[time, setTime] = useState(12);
 
     useEffect(() => {
+        const getLocation = () => {
+            return new Promise((resolve, reject) => {
+                navigator.geolocation.getCurrentPosition(resolve, reject);
+            })
+        }
         const fetchData = async () => {
+            const todayDate = new Date();
+            const todayDateFormatted = todayDate.toJSON().slice(0, 10).replace(/-/g, '-');
+            const nextWeekDate = new Date(todayDate.getTime() + 7 * 24 * 60 * 60 * 1000).toJSON().slice(0, 10).replace(/-/g, '-');
+            const timeNow = todayDate.getHours();
+            setTime(timeNow)
             setIsLoading(true);
-            const data = await getDailyWeather(40.0, 74.0);
-            setCurrentWeather(data);
+            const loc = await getLocation();
+            const todayData = await getWeather(loc.coords.latitude, loc.coords.longitude, todayDateFormatted, todayDateFormatted);
+            const allData = await getWeather(loc.coords.latitude, loc.coords.longitude, todayDateFormatted, nextWeekDate)
+            const locationName = await getLocationFromCoords(loc.coords.latitude, loc.coords.longitude);
+            setDailyWeather(todayData);
+            setallWeather(allData);
+            setLocationFromCoords(locationName);
             setIsLoading(false)
-            console.log(data);
         }
         fetchData();
     }, [])
@@ -25,44 +42,43 @@ export const Weather = () => {
                 <div className="current-weather-container">
                     <div className="current-weather border-shadow">
                         <h2 className="weather-title">Current Weather</h2>
-                        <span className="location">Placeholder, USA</span>
+                        <span className="location">{locationFromCoords.address.city}, {locationFromCoords.address.state}</span>
                         <span className="temperature">
-                            {parseInt(currentWeather.hourly.temperature_2m[0])}°F
+                            {parseInt(dailyWeather.hourly.temperature_2m[time])}°F
                         </span>
-                        <span className="high-low">
+                        <div className="high-low">
                             <span style={{ display: 'block' }}>High & Low</span>
-                            {parseInt(currentWeather.daily.temperature_2m_max[0])}°F • {parseInt(currentWeather.daily.temperature_2m_min[0])}°F
-                            <span style={{ float: 'right', paddingRight: '1em' }}>Chance of precipitation: {currentWeather.hourly.precipitation[0]}%</span>
-                        </span>
-                        
+                            <span>{parseInt(allWeather.daily.temperature_2m_max[0])}°F • {parseInt(dailyWeather.daily.temperature_2m_min[0])}°F</span>
+                            <span style={{ float: 'right', paddingRight: '1em' }}>Chance of precipitation: {dailyWeather.hourly.precipitation[0]}%</span>
+                        </div>
                     </div>
                 </div>
                 <div className="three-day-weather-container">
                     <div className="three-day-weather border-shadow">
                         <h2 className="weather-title">Three Day Weather</h2>
-                        <span className="location">Placeholder, USA</span>
+                        <span className="location">{locationFromCoords.address.city}, {locationFromCoords.address.state}</span>
                         <div className="weather-days">
                             <div className="three-day">
                                 <span className="day-title">Today</span>
                                 <div className="three-day-info">
-                                    <span>High {parseInt(currentWeather.daily.temperature_2m_max[0])}°F</span>
-                                    <span>Low {parseInt(currentWeather.daily.temperature_2m_min[0])}°F</span>
+                                    <span>High {parseInt(allWeather.daily.temperature_2m_max[0])}°F</span>
+                                    <span>Low {parseInt(allWeather.daily.temperature_2m_min[0])}°F</span>
                                 </div>
                             </div>
                             <div className="vertical-divider"></div>
                             <div className="three-day">
                                 <span className="day-title">Tomorrow</span>
                                 <div className="three-day-info">
-                                    <span>High {parseInt(currentWeather.daily.temperature_2m_max[1])}°F</span>
-                                    <span>Low {parseInt(currentWeather.daily.temperature_2m_min[1])}°F</span>
+                                    <span>High {parseInt(allWeather.daily.temperature_2m_max[1])}°F</span>
+                                    <span>Low {parseInt(allWeather.daily.temperature_2m_min[1])}°F</span>
                                 </div>
                             </div>
                             <div className="vertical-divider"></div>
                             <div className="three-day">
                                 <span className="day-title">Day After Tomorrow</span>
                                 <div className="three-day-info">
-                                    <span>High {parseInt(currentWeather.daily.temperature_2m_max[2])}°F</span>
-                                    <span>Low {parseInt(currentWeather.daily.temperature_2m_min[2])}°F</span>
+                                    <span>High {parseInt(allWeather.daily.temperature_2m_max[2])}°F</span>
+                                    <span>Low {parseInt(allWeather.daily.temperature_2m_min[2])}°F</span>
                                 </div>
                             </div>
 
@@ -72,7 +88,7 @@ export const Weather = () => {
                 <div className="seven-day-weather-container">
                     <div className="seven-day-weather border-shadow">
                         <h2 className="weather-title">Seven Day Weather</h2>
-                        <span className="location">Placeholder, USA</span>
+                        <span className="location">{locationFromCoords.address.city}, {locationFromCoords.address.state}</span>
                         <div className="seven-day-weather-list">
                             <div className="horizontal-divider"></div>
                             <div className="seven-day">
--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
-export const getDailyWeather = async (latitude, longitude) => {
+export const getWeather = async (latitude, longitude, startDate, endDate) => {
     try {
-        const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${longitude}longitude=${longitude}&hourly=temperature_2m,precipitation&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York`)
+        const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation&daily=temperature_2m_max,temperature_2m_min&temperature_unit=fahrenheit&timezone=America%2FNew_York&start_date=${startDate}&end_date=${endDate}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getLocationFromCoords = async (latitude, longitude) => {
+    try {
+        const response = await axios.get(`https://forward-reverse-geocoding.p.rapidapi.com/v1/reverse`, {
+            params: {
+                lat: latitude,
+                lon: longitude,
+            },
+            headers: {
+                'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+                'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
+            }
+        });
         return response.data;
     } catch (error) {
         console.log(error);
